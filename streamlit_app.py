60d80a5ecb03e0fcae51936038edad18be38e081
zznba2k <94033650+zznba2k@users.noreply.github.com>
2023-03-18 19:09:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import av
 import threading
 import streamlit as st
 import streamlit_nested_layout
-from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer,WebRtcMode,RTCConfiguration
+from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
 
 from audio_handling import AudioFrameHandler
 from drowsy_detection import VideoFrameHandler
@@ -77,9 +77,8 @@ with col1:
         key="drowsiness-detection",
         video_frame_callback=video_frame_callback,
         audio_frame_callback=audio_frame_callback,
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}),  # Add this to config for cloud deployment.
-        media_stream_constraints={"video": {"height": {"ideal": 480}}, "audio": True},
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this to config for cloud deployment.
+        media_stream_constraints={"video": {"height": {"ideal": 520}}, "audio": True},
         video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
     )
 
